62f4e2b4378f5112125984d6ec0b5f4c829f09cc
TawhidulIslamRefat <tirefat544@gamil.com>
2026-01-18 19:25:47 +0600

    Fix Error

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -4,12 +4,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyAygHBcL5hH00k0MUDwmaVWfcx2DksovWg",
+  authDomain: "meal-mate-login.firebaseapp.com",
+  projectId:"meal-mate-login",
+  storageBucket: "meal-mate-login.appspot.com",
+  messagingSenderId:"177612173997",
+  appId: "1:177612173997:web:70490a78bf0e6738a70c24",
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
